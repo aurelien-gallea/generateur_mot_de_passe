695669335e2aeb90b68bd296b35d5c165e3945d9
aurelien-gallea <aurelien.gallea@laplateforme.io>
2023-07-22 20:33:45 +0200

    ajout du projet

---
+++ Outils.cs
@@ -0,0 +1,40 @@
+﻿namespace formationCS
+{
+    static class Outils
+    {
+        public static int DemanderNbPositif(string question)
+        {
+            return NbRange(question, 1, int.MaxValue);
+        }
+        public static int NbRange(string question, int min, int max)
+        {
+            int longueurMDP = DemanderNombre(question);
+            if (longueurMDP >= min && longueurMDP <= max)
+            {
+                return longueurMDP;
+            }
+            Console.WriteLine("ERREUR : le nombre doit être compris entre " + min + " et " + max);
+            Console.WriteLine();
+            return NbRange(question, min, max);
+        }
+        public static int DemanderNombre(string question)
+        {
+            int reponse = 0;
+            while (true)
+            {
+                Console.Write(question);
+                try
+                {
+                    reponse = int.Parse(Console.ReadLine());
+                    return reponse;
+                }
+                catch
+                {
+                    Console.WriteLine("entrez un chiffre valide !");
+                    Console.WriteLine();
+                }
+            }
+
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,65 @@
+﻿using formationCS;
+
+namespace generateur_mot_de_passe
+{
+    internal class Program
+    {
+
+        static void Main(string[] args)
+        {
+            int longueur = Outils.DemanderNbPositif("Longueur du mot de passe : ");
+            string minuscules = "abcdefghijklmnopqrstuvwxyz";
+            string majuscules = minuscules.ToUpper();
+            string chiffres = "0123456789";
+            string caracteresSpeciaux = "#&+-@^_";
+            string alphabet = "";
+            int choix = 0;
+
+
+            while (choix < 1 || choix > 4)
+            {
+                choix = Outils.NbRange("Vous voulez un mot de passe avec : " +
+                    "\n1 - Uniquement des caractères en minuscules" +
+                    "\n2 - Des caractères minuscules et majuscules" +
+                    "\n3 - Des caractères et des chiffres" +
+                    "\n4 - Caractères, chiffres et caractères spéciaux" +
+                    "\nVotre choix : ", 1, 4);
+            }
+
+            switch (choix)
+            {
+                case 1:
+                    alphabet = minuscules;
+                    break;
+                case 2:
+                    alphabet = minuscules + majuscules;
+                    break;
+                case 3:
+                    alphabet = minuscules + majuscules + chiffres;
+                    break;
+                case 4:
+                    alphabet = minuscules + majuscules + chiffres + caracteresSpeciaux;
+                    break;
+                default:
+                    break;
+            }
+
+            Random rand = new Random();
+            const int NB_MDP = 10;
+
+            for (int i = 0; i < NB_MDP; i++)
+            {
+                string motDePasse = "";
+
+
+                for (int j = 0; j < longueur; j++)
+                {
+                    int indexAleatoire = rand.Next(0, alphabet.Length);
+                    char lettreAleatoire = alphabet[indexAleatoire];
+                    motDePasse += lettreAleatoire;
+                }
+                Console.WriteLine("mot de passe : " + motDePasse);
+            }
+        }
+    }
+}
